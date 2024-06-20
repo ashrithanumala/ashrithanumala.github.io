b3b78d58f4e5941b0de3de49f83da47a38db7d05
ashrithanumala <64055781+ashrithanumala@users.noreply.github.com>
2024-06-20 04:03:53 +0000

    Deploying to gh-pages from @ ashrithanumala/ashrithanumala.github.io@015a2451ffe862e6a7a5e292b742971f3f348f62 🚀

---
+++ assets/js/slideshow.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",function(){function e(){let n=document.getElementsByClassName("mySlides");for(let e=0;e<n.length;e++)n[e].style.display="none";++t>n.length&&(t=1),n[t-1].style.display="block",setTimeout(e,2e3)}let t=0;e()});
