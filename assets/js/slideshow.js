5f6393343ed2c39875960536851c43edb823017e
ashrithanumala <64055781+ashrithanumala@users.noreply.github.com>
2024-07-27 02:49:58 +0000

    Deploying to gh-pages from @ ashrithanumala/ashrithanumala.github.io@febd52cbbcbe028b12e2a73d2d86e2835063e425 🚀

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -1,1 +1,1 @@
-document.addEventListener("DOMContentLoaded",function(){function e(){let n=document.getElementsByClassName("mySlides");if(0!==n.length){for(let e=0;e<n.length;e++)n[e].style.display="none";++t>n.length&&(t=1),n[t-1].style.display="block",setTimeout(e,2e3)}}let t=0;e()});
+document.addEventListener("DOMContentLoaded",function(){function e(){let n=document.getElementsByClassName("mySlides");if(0!==n.length){console.log(n.length);for(let e=0;e<n.length;e++)n[e].style.display="none";++t>n.length&&(t=1),n[t-1].style.display="block",setTimeout(e,2e3)}}let t=0;e()});
